d5423bfe8f3d79ab4f348d49643638330af2cc6b
Nóra Szepes <lordblendi@gmail.com>
2023-10-19 17:23:53 +0200

    added reduc toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from "react"
+import { Provider } from "react-redux"
 import { createRoot } from "react-dom/client"
+
+import { store } from "./store"
+
+import Counter from "./components/counter/components/Counter"
 
 import "./style/index.scss"
 
@@ -11,10 +16,15 @@ if (!!rootElement) {
             <div>
                 <h1 className="tw-bg-yellow-200">Hello!!</h1>
                 <h2>Welcome to your First React App..!</h2>
+                <Counter />
             </div>
         )
     }
 
     const root = createRoot(rootElement)
-    root.render(<App />)
+    root.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    )
 }
